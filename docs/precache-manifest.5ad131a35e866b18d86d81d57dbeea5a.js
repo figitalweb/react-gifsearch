1d5592c01318f6a452bc3a9aac26098d77dcc44b
Diego <die.11.91@gmail.com>
2020-10-07 14:21:49 -0300

    docs agregado

---
+++ docs/precache-manifest.5ad131a35e866b18d86d81d57dbeea5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f14f9da176fdafe9172b89159839c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c661ecdb7bb39f85a204",
+    "url": "/static/css/main.76c4a7fc.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c661ecdb7bb39f85a204",
+    "url": "/static/js/main.acc41873.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
